38abf4369fef5aa6888ba6cf229ad5a960d07475
n1wz <nlwz@proton.me>
2023-01-13 05:50:36 -0800

    fixed shell generation

--- shellcode.cpp
+++ shellcode.cpp
@@ -4,22 +4,22 @@
 #include "shellcode.h"
 
 enum {
-	ADD, SUB/*, AND, OR, XOR*/
+	ADD, SUB, AND, OR, XOR
 };
 
 int shellcode::generate(int eax, unsigned char* out) {
 	std::vector<std::pair<std::string, int>> instr {
-		{ "\x05", ADD }, { "\x2D", SUB }/*, {"\x25", AND}, {"\x0D", OR}, {"\x35", XOR},*/
+		{ "\x05", ADD }, { "\x2D", SUB }, {"\x25", AND}, {"\x0D", OR}, {"\x35", XOR}
 	};
-	int total_instr = utils::rand(5, 15), size = total_instr * 5 + 5, out_s = 0;
-	unsigned int eax_v = INT_MAX;
+	int total_instr = utils::rand(5, 15), out_s = 2;
+	unsigned int eax_v = 0;
 
 	for (int i = 0; i < total_instr; i++) {
 		int cur_instr = utils::rand(0, instr.size() - 1), num = 0;
 
 		switch (instr[cur_instr].second) {
 			case ADD: {
-				num = utils::rand(0, INT_MAX - eax_v);
+				num = utils::rand(0, UINT_MAX - eax_v);
 				eax_v += num;
 				break;
 			}
@@ -28,30 +28,44 @@ int shellcode::generate(int eax, unsigned char* out) {
 				eax_v -= num;
 				break;
 			}
-			/*case AND: {
+			case AND: {
+				num = utils::rand(0, UINT_MAX);
 				eax_v &= num;
 				break;
 			}
 			case OR: {
+				num = utils::rand(0, UINT_MAX);
 				eax_v |= num;
 				break;
 			}
 			case XOR: {
+				num = utils::rand(0, UINT_MAX);
 				eax_v ^= num;
 				break;
-			}*/
+			}
 		}
 
-		memcpy(out + size - out_s - 5, instr[cur_instr].first.c_str(), instr[cur_instr].first.size());
-		memcpy(out + size - out_s - 4, &num, 4);
+		memcpy(out + out_s, instr[cur_instr].first.c_str(), instr[cur_instr].first.size());
+		memcpy(out + out_s + 1, &num, 4);
 		out_s += instr[cur_instr].first.size() + 4;
 	}
 
-	int n = eax_v - eax;
-	memcpy(out, "\xB8\xFF\xFF\xFF\x7F", 5); // mov eax, 0x7fffffff
-	memcpy(out + out_s, "\x2D", 1);
-	memcpy(out + 1, &n, 4);
-	out_s += 5;
+	if (eax_v > eax) {
+		unsigned int n = eax_v - eax;
+		memcpy(out + out_s, "\x2D", 1);
+		memcpy(out + out_s + 1, &n, 4);
+		out_s += 5;
+	}
+	else if (eax_v < eax) {
+		unsigned int n = eax - eax_v;
+		memcpy(out + out_s, "\x05", 1);
+		memcpy(out + out_s + 1, &n, 4);
+		out_s += 5;
+	}
+
+	memcpy(out, "\x31\xC0", 2); // xor eax, eax
+	memcpy(out + out_s, "\xFF\xD0\xC3", 3); // call eax \n ret
+	out_s += 3;
 
 	return out_s;
 }
